7c6127a4e34bca7b1c9dd8b225979f561054c8f0
Leonagarabedian <leonagarabedian@yahoo.com>
2026-09-10 09:59:09 +0300

    fix grid video mask transparency

--- src/modules/grid-video-reveal.js
+++ src/modules/grid-video-reveal.js
@@ -15,7 +15,7 @@ function encodeMask(cols, rows, cells, gap) {
     })
     .join("");
 
-  const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${cols} ${rows}" preserveAspectRatio="none" shape-rendering="crispEdges"><rect width="100%" height="100%" fill="black"/>${rects}</svg>`;
+  const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${cols} ${rows}" preserveAspectRatio="none" shape-rendering="crispEdges">${rects}</svg>`;
   return `url("data:image/svg+xml,${encodeURIComponent(svg)}")`;
 }
 
