7d44d25eca42061a03f504298d2ab91772e9b9ff
Leonagarabedian <leonagarabedian@yahoo.com>
2026-09-13 14:45:46 +0300

    Register footer reveal composition

--- src/modules/registry.js
+++ src/modules/registry.js
@@ -11,6 +11,7 @@ import { depthEmerge } from "./depth-emerge.js";
 import { elementBlurReveal } from "./element-blur-reveal.js";
 import { elementLayoutReturn } from "./element-layout-return.js";
 import { flipRelocation } from "./flip-relocation.js";
+import { footerReveal } from "./footer-reveal.js";
 import { gridVideoReveal } from "./grid-video-reveal.js";
 import { heroFrameTransition } from "./hero-frame-transition.js";
 import { heroHeartTransition } from "./hero-heart-transition.js";
@@ -70,6 +71,7 @@ export const modules = [
   linkSwap,
   navFlip,
   brandLoad,
+  footerReveal,
   responsiveMenu,
   loaderComposition,
   pageTransition,
