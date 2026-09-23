a93101e21b9a9195aa0d80eafd135a51c3213f8b
Leonagarabedian <leonagarabedian@yahoo.com>
2026-09-23 10:13:45 +0300

    Register selected character Y rotation under clear name

--- src/modules/registry.js
+++ src/modules/registry.js
@@ -2,7 +2,7 @@ import { accordionMedia } from "./accordion-media.js";
 import { architectureSpace } from "./architecture-space.js";
 import { blurReveal } from "./blur-reveal.js";
 import { brandFieldActivate } from "./brand-field-activate.js";
-import { brandLoad } from "./brand-load.js";
+import { selectedCharacterYRotation } from "./selected-character-y-rotation.js";
 import { brandaSpatialPinLayout } from "./branda-spatial-pin-layout.js";
 import { brandaSpatialShell } from "./branda-spatial-shell.js";
 import { brandaSpatialWorks } from "./branda-spatial-works.js";
@@ -111,7 +111,7 @@ export const modules = [
   cursor,
   linkSwap,
   navFlip,
-  brandLoad,
+  selectedCharacterYRotation,
   brandFieldActivate,
   footerReveal,
   responsiveMenu,
